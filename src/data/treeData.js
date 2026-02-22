7da2e3af8bf15902614a4f9d25866e7083960686
Amjad <amjadibrahim218@gmail.com>
2026-02-22 21:13:30 +0200

    ok

--- src/data/treeData.js
+++ src/data/treeData.js
@@ -1,14 +1,11 @@
 // Import local images
 import mohamedAlkadyImg from '../assets/images/people/mohamed-alkady.jpg';
-import amanyImg from '../assets/images/people/amany.jpg';
-import nadaImg from '../assets/images/people/nada.jpg';
 import khadijaImg from '../assets/images/people/khadija.jpg';
 import mohamedSayedImg from '../assets/images/people/mohamed-sayed.jpg';
 import amjadImg from '../assets/images/people/amjad.jpg';
 import ghadaImg from '../assets/images/people/ghada.jpg';
 import dianaImg from '../assets/images/people/diana.jpg';
 import zahraaImg from '../assets/images/people/zahraa.jpg';
-import ahmedYasserImg from '../assets/images/people/ahmed-yasser.jpg';
 
 export const treeData = {
   roots: [
@@ -19,21 +16,9 @@ export const treeData = {
       linkedin: 'https://www.linkedin.com/in/mohamedalkady-cp/',
       children: [
         {
-          id: 'node1',
-          name: 'أمانى محمد',
-          avatar: amanyImg,
-          linkedin: 'https://www.linkedin.com/in/amanymsadky/'
-        },
-        {
-          id: 'node2',
-          name: 'ندى حسنين',
-          avatar: nadaImg,
-          linkedin: 'https://www.linkedin.com/in/nada-hassanin-5989a933b/'
-        },
-        {
           id: 'node3',
           name: 'رؤى هشام',
-          avatar: null, // مفيش صورة لرؤى في المجلد
+          avatar: null,
           linkedin: 'https://www.linkedin.com/in/roaa-hisham-sharafeldin-5182b9280/'
         },
         {
@@ -45,7 +30,7 @@ export const treeData = {
         {
           id: 'node5',
           name: 'عمر صلاح',
-          avatar: null, // مفيش صورة لعمر في المجلد
+          avatar: null,
           linkedin: 'https://www.linkedin.com/in/omar-matar-44220031a/'
         },
         {
@@ -79,12 +64,6 @@ export const treeData = {
           name: 'الزهراء نور',
           avatar: zahraaImg,
           linkedin: 'https://www.linkedin.com/in/al-zahraa-nour-70047b380/'
-        },
-        {
-          id: 'node10',
-          name: 'أحمد ياسر',
-          avatar: ahmedYasserImg,
-          linkedin: 'https://www.linkedin.com/in/ahmed-yasser-358536297'
         }
       ]
     }
